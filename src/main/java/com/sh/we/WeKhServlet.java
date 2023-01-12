2d7e6a84b3f4ba157c34392214558621ed0b8a00
aw1786 <aw1786@naver.com>
2023-01-12 12:46:45 +0900

    WeKhServlet 서블릿 추가

---
+++ src/main/java/com/sh/we/WeKhServlet.java
@@ -0,0 +1,26 @@
+package com.sh.we;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WeKhServlet
+ */
+@WebServlet("/hello")
+public class WeKhServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath())
+		.append("kh가 /hello에 응답했습니다.");
+	}
+
+}
